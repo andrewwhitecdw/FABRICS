b3bbc4f0297e25b348a15116e2087ac869cd53a5
Andrew White <andrewh@cdw.com>
2026-07-27 07:35:59 -0500

    fix(robot-visualizer): robot regex lacks group, misses (+1 more)

--- src/fabrics_sim/visualization/robot_visualizer.py
+++ src/fabrics_sim/visualization/robot_visualizer.py
@@ -65,12 +65,12 @@ class RobotVisualizer():
         self.simulation_context.initialize_physics()
 
         # Create articulation view which we will use to teleport the robot joints
-        robot_range = "/World/Robot_[1-9]|[1-9][0-9]{1,3}|9000"
+        robot_range = "/World/Robot_([1-9]|[1-9][0-9]{1,2}|[1-8][0-9]{3}|9000)"
         self.robots_view =\
             ArticulationView(prim_paths_expr=robot_range, name="robots_view")
         self.robots_view.initialize()
         self.robots_view.set_enabled_self_collisions(np.array([False] * batch_size))
-        self.robots_view.set_body_disable_gravity(np.zeros((batch_size, self.robots_view.num_bodies)))
+        self.robots_view.set_body_disable_gravity(np.zeros((batch_size, self.robots_view.num_bodies), dtype=bool))
 
         # Add robot view to world
         self.world.scene.add(self.robots_view)
